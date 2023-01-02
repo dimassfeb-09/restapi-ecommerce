c1a6a8697dcae30c1f73aa2b4c3dc3e0519a5494
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-01-02 22:33:37 +0700

    Users

--- controller/users_controller.go
+++ controller/users_controller.go
@@ -1,1 +1,171 @@
 package controller
+
+import (
+	"fmt"
+	"github.com/dimassfeb-09/restapi-ecommerce.git/entity/request"
+	"github.com/dimassfeb-09/restapi-ecommerce.git/entity/web"
+	"github.com/dimassfeb-09/restapi-ecommerce.git/exception"
+	"github.com/dimassfeb-09/restapi-ecommerce.git/helpers"
+	"github.com/dimassfeb-09/restapi-ecommerce.git/services"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+	"strconv"
+)
+
+type UserController interface {
+	CreateUser(c *gin.Context)
+	UpdateUser(c *gin.Context)
+	FindByIdUser(c *gin.Context)
+	FindByUsername(c *gin.Context)
+	DeleteUser(c *gin.Context)
+	FindAllUser(c *gin.Context)
+}
+
+type UserControllerImpl struct {
+	UserService services.UserServices
+}
+
+func NewUserControllerImpl(userService services.UserServices) UserController {
+	return &UserControllerImpl{UserService: userService}
+}
+
+func (u *UserControllerImpl) CreateUser(c *gin.Context) {
+
+	var user *request.CreateUserRequest
+	if err := c.ShouldBind(&user); err != nil {
+		msg := exception.ToErrorMsg(err.Error(), exception.BadRequest)
+		exception.ErrorHandler(c, msg)
+		return
+	}
+
+	if msgErr := helpers.ValidatorRequest(user); msgErr != nil {
+		msg := exception.ToErrorMsg(msgErr[0], exception.BadRequest)
+		exception.ErrorHandler(c, msg)
+	}
+
+	validate := validator.New()
+	if err := validate.Struct(user); err != nil {
+		var msgErr []string
+		for _, err := range err.(validator.ValidationErrors) {
+			msgErr = append(msgErr, "Error with field "+err.Field()+" where Tag "+err.Tag())
+		}
+		msg := exception.ToErrorMsg(msgErr[0], exception.BadRequest)
+		exception.ErrorHandler(c, msg)
+		return
+	}
+
+	if createResponse, err := u.UserService.CreateUser(c.Request.Context(), user); err != nil { // if failed
+		msg := exception.ToErrorMsg(err.Msg, err.Error)
+		exception.ErrorHandler(c, msg)
+		return
+	} else { // if success
+		c.JSON(http.StatusOK, helpers.ToWebResponse(http.StatusOK, "OK", createResponse))
+		return
+	}
+
+}
+
+func (u *UserControllerImpl) UpdateUser(c *gin.Context) {
+
+	id := c.Param("id")
+	dataId, _ := strconv.Atoi(id)
+
+	var user *request.UpdateUserRequest
+	if err := c.ShouldBind(&user); err != nil {
+		msg := exception.ToErrorMsg(err.Error(), exception.BadRequest)
+		exception.ErrorHandler(c, msg)
+		return
+	}
+
+	if msgErr := helpers.ValidatorRequest(user); msgErr != nil {
+		msg := exception.ToErrorMsg(msgErr[0], exception.BadRequest)
+		exception.ErrorHandler(c, msg)
+	}
+
+	user.Id = dataId
+	if updateUser, err := u.UserService.UpdateUser(c.Request.Context(), user); err != nil {
+		msg := exception.ToErrorMsg(err.Msg, err.Error)
+		exception.ErrorHandler(c, msg)
+		return
+	} else {
+		c.JSON(http.StatusOK, helpers.ToWebResponse(http.StatusOK, "OK", updateUser))
+	}
+
+}
+
+func (u *UserControllerImpl) FindByIdUser(c *gin.Context) {
+
+	id := c.Param("id")
+	dataId, err := strconv.Atoi(id)
+	if err != nil {
+		numError := err.(*strconv.NumError)
+		s := numError.Err.Error()
+		if s == "invalid syntax" {
+			msg := fmt.Sprintf("Errors: %s bukan type data number.", numError.Num)
+			errMsg := exception.ToErrorMsg(msg, exception.BadRequest)
+			exception.ErrorHandler(c, errMsg)
+			return
+		}
+	}
+
+	user, errs := u.UserService.FindByIdUser(c.Request.Context(), dataId)
+	if errs != nil {
+		msg := exception.ToErrorMsg(errs.Msg, errs.Error)
+		exception.ErrorHandler(c, msg)
+		return
+	}
+
+	c.JSON(http.StatusOK, web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   user,
+	})
+}
+
+func (u *UserControllerImpl) FindByUsername(c *gin.Context) {
+
+}
+
+func (u *UserControllerImpl) DeleteUser(c *gin.Context) {
+
+	id := c.Param("id")
+	dataId, err := strconv.Atoi(id)
+	if err != nil {
+		numError := err.(*strconv.NumError)
+		s := numError.Err.Error()
+		if s == "invalid syntax" {
+			msg := fmt.Sprintf("Errors: %s bukan type data number.", numError.Num)
+			errMsg := exception.ToErrorMsg(msg, exception.BadRequest)
+			exception.ErrorHandler(c, errMsg)
+			return
+		}
+	}
+
+	if errs := u.UserService.DeleteUser(c.Request.Context(), dataId); errs != nil {
+		msg := exception.ToErrorMsg(errs.Msg, errs.Error)
+		exception.ErrorHandler(c, msg)
+		return
+	}
+
+	c.JSON(http.StatusOK, web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   "Berhasil menghapus data dengan ID " + id,
+	})
+}
+
+func (u *UserControllerImpl) FindAllUser(c *gin.Context) {
+
+	if users, errMsg := u.UserService.FindAllUser(c.Request.Context()); errMsg != nil {
+		err := exception.ToErrorMsg(errMsg.Msg, errMsg.Error)
+		exception.ErrorHandler(c, err)
+	} else {
+		c.JSON(http.StatusOK, web.WebResponse{
+			Code:   http.StatusOK,
+			Status: "OK",
+			Data:   users,
+		})
+	}
+
+}
--- repository/users_repository.go
+++ repository/users_repository.go
@@ -1,1 +1,126 @@
-package userRepository
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/dimassfeb-09/restapi-ecommerce.git/entity/domain"
+)
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, DB *sql.Tx, users *domain.Users) (*domain.Users, error)
+	FindByIdUser(ctx context.Context, DB *sql.DB, userId int) (*domain.Users, error)
+	FindByUsername(ctx context.Context, DB *sql.DB, username string) (bool, error)
+	UpdateUser(ctx context.Context, DB *sql.Tx, users *domain.Users) (*domain.Users, error)
+	DeleteUser(ctx context.Context, DB *sql.Tx, userId int) error
+	FindAll(ctx context.Context, DB *sql.DB) ([]*domain.Users, error)
+}
+
+type UserRepositoryImpl struct {
+}
+
+func NewUserRepositoryImpl() UserRepository {
+	return &UserRepositoryImpl{}
+}
+
+func (u *UserRepositoryImpl) CreateUser(ctx context.Context, DB *sql.Tx, users *domain.Users) (*domain.Users, error) {
+
+	var Sql string = "INSERT INTO users(name, username, password) VALUES (?, ?, ?)"
+	rows, err := DB.ExecContext(ctx, Sql, users.Name, users.Username, users.Password)
+	if err != nil {
+		errMsg := fmt.Errorf("Error Create User: %s", err.Error())
+		return nil, errMsg
+	}
+
+	lastId, err := rows.LastInsertId() // Getting last Id in Database
+	if err != nil {
+		errMsg := fmt.Errorf("Error Last ID: %s", err.Error())
+		return nil, errMsg
+	}
+
+	user := &domain.Users{
+		ID:        int(lastId),
+		Name:      users.Name,
+		Username:  users.Username,
+		Balance:   0,
+		CreatedAt: users.CreatedAt,
+	}
+
+	return user, nil
+}
+
+func (u *UserRepositoryImpl) FindByIdUser(ctx context.Context, DB *sql.DB, userId int) (*domain.Users, error) {
+	rows, err := DB.QueryContext(ctx, "SELECT * FROM users WHERE id = ?", userId)
+	if err != nil {
+		errMsg := fmt.Errorf("Error Query Find User By ID: %s", err.Error())
+		return nil, errMsg
+	}
+	defer rows.Close()
+
+	var user domain.Users
+	if rows.Next() { // if data founded
+		err := rows.Scan(&user.ID, &user.Name, &user.Username, &user.Password, &user.Balance, &user.CreatedAt)
+		if err != nil {
+			errMsg := fmt.Errorf("Error Scan User Find By ID: %s", err)
+			return nil, errMsg
+		}
+		return &user, nil
+	} else { // if data not founded
+		errMsg := fmt.Errorf("User dengan ID %d tidak ditemukan.", userId)
+		return nil, errMsg
+	}
+}
+
+func (u *UserRepositoryImpl) FindByUsername(ctx context.Context, DB *sql.DB, username string) (bool, error) {
+	rows, err := DB.QueryContext(ctx, "SELECT username FROM users WHERE username = ?", username)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		errMsg := fmt.Errorf("Username %s tidak ditemukan.", username)
+		return false, errMsg
+	}
+
+	return true, nil
+}
+
+func (u *UserRepositoryImpl) UpdateUser(ctx context.Context, DB *sql.Tx, users *domain.Users) (*domain.Users, error) {
+	SQL := "UPDATE users SET name = ?, username = ?, password = ?, balance = ? WHERE id = ?"
+
+	_, err := DB.ExecContext(ctx, SQL, &users.Name, &users.Username, &users.Password, &users.Balance, &users.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (u *UserRepositoryImpl) DeleteUser(ctx context.Context, DB *sql.Tx, userId int) error {
+	if _, err := DB.ExecContext(ctx, "DELETE FROM users WHERE id = ?", userId); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *UserRepositoryImpl) FindAll(ctx context.Context, DB *sql.DB) ([]*domain.Users, error) {
+	rows, err := DB.QueryContext(ctx, "SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []*domain.Users
+	for rows.Next() {
+		var user domain.Users
+		if err := rows.Scan(&user.ID, &user.Name, &user.Username, &user.Password, &user.Balance, &user.CreatedAt); err != nil {
+			return nil, err
+		} else {
+			users = append(users, &user)
+		}
+	}
+
+	return users, nil
+}
