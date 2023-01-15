671c6f30f69ef33bb558ecfba260ad66e4410def
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-01-15 08:42:59 +0700

    Database Config

--- config/database.go
+++ config/database.go
@@ -1,1 +1,17 @@
-package controller
+package config
+
+import (
+	"database/sql"
+	"time"
+)
+
+func DBConnection() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/ecommerce?parseTime=true")
+
+	db.SetMaxOpenConns(50)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db, err
+}
