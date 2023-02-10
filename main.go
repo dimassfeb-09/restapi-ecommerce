80fe7bf2788d94536cf3b52592f54fb43ed80c67
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-02-10 18:44:25 +0700

    main.go updated

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package restapi_ecommerce
+package main
+
+import (
+	"github.com/dimassfeb-09/restapi-ecommerce.git/app"
+	"github.com/dimassfeb-09/restapi-ecommerce.git/middleware"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+
+	db, err := app.DBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	r := gin.Default()
+
+	r.HandleMethodNotAllowed = true
+	r.NoRoute(middleware.NoRoute)
+	r.Use(middleware.AllowAccessMiddleware)
+
+	initializer := app.NewInitialized(db)
+	app.RoutesApi(initializer, r)
+
+	r.Run(":3000")
+}
